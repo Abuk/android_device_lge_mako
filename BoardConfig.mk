c50a0801f286d3fcd32f88079dae7570c1ebe690
zaclimon <isaacpateau05@gmail.com>
2016-08-23 20:01:21 -0400

    BoardConfig: Replace MALLOC_IMPL with MALLOC_SVELTE
    MALLOC_IMPL is deprecated and let's use MALLOC_SVELTE.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,6 +131,6 @@ USE_MINIKIN := true
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
-MALLOC_IMPL := dlmalloc
+MALLOC_SVELTE := true
 
 -include vendor/lge/mako/BoardConfigVendor.mk
