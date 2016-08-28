2ea1e58e2f765d79aad9ce7769381fb3160c470c
spezi77 <spezi77@gmx.com>
2016-08-28 10:11:48 +0200

    BoardConfig: Enable text relocations.
    * needed for camera
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,9 @@ HAVE_ADRENO_SOURCE:= false
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
 
+# Camera
+TARGET_NEEDS_TEXT_RELOCATIONS := true
+
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
